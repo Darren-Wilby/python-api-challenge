2dc32cc28e0c7d81146f759e3c1467b807ff2ef8
Darren <darren.wilby10@gmail.com>
2023-10-18 23:44:48 +0100

    feat: added github actions CI/CD

--- kubernetes/apply_yaml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
--- react-frontend/src/components/Button.js
+++ react-frontend/src/components/Button.js
@@ -2,9 +2,7 @@ import React from 'react'
 import { Button } from 'semantic-ui-react'
 
 const SubmitButton = () => (
-  <div>
-    <Button icon='right arrow' size='large' circular compact positive></Button>
-  </div>
+  <div><Button icon='right arrow' size='huge' circular compact positive></Button></div>
 )
 
 export default SubmitButton
